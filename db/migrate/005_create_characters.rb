5dbad922c5e56822c6d2f21fbcafce4c56314cb8
Brad Hambrice <hambrice@uab.edu>
2018-01-23 01:57:30 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,8 +3,8 @@ class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :show_id
-      t.string :actor_id
+      #t.string :show_id
+      #t.string :actor_id
     end
   end
 end
