ccb27cf56641809c1278bd83b3526501bc9b2ba7
Brad Hambrice <hambrice@uab.edu>
2018-01-23 02:14:40 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.each do |c|
-      binding.pry
+      #binding.pry
       puts "#{c.name} - #{c.show.name}"
     end
   end
